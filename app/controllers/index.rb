53efae14b7437e6a2010485995c657b06e0d9a87
Srinivas Rao <raorao@rao.local>
2014-06-30 10:38:35 -0700

    adds post/gadgets route

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,3 +2,15 @@ get '/gadgets' do
   @gadgets = Gadget.all
   erb :'gadgets/index'
 end
+
+post '/gadgets' do
+  gadget = Gadget.new(name: params[:name], body: params[:body])
+
+  if gadget.save
+    redirect to('/gadgets')
+  else
+    status 422
+  end
+
+end
+
